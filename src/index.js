01421dc39ae16362dbb4a39f64deab52a8f49869
Sergiy Nimerovsky <sergiynimerovsky@Sergiys-MacBook-Pro.local>
2021-09-07 15:59:31 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Images
+import './images.js'
+
+// Styles
+import "./scss/app.scss";
+import "./scss/utils.scss";
+
+// App Scripts
+import './services/main_script.js'
+// import './services/seo.js'
+import './services/utm.js'
