1d63438d9add81e6742429a8bdb4b742af4dcea0
Sergiy Nimerovsky <sergiynimerovsky@Sergiys-MacBook-Pro.local>
2021-10-27 13:22:13 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// Images
+import './images.js'
+
+// Styles
+import "./scss/owl.carousel.min.scss";
+import "./scss/owl.theme.default.min.scss";
+import "./scss/slick.scss";
+import "./scss/app.scss";
+import "./scss/utils.scss";
+
+// // App Scripts
+// import './services/main_script.js'
+// import './services/seo.js'
+// import './services/utm.js'
