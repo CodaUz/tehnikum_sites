5addd932dc5d22abd4c750cfdefb9dfcefaedeef
Sergiy Nimerovsky <sergiynimerovsky@Sergiys-MacBook-Pro.local>
2021-11-03 11:43:51 +0200

    add fixes

--- dist/services/main_script.js
+++ dist/services/main_script.js
@@ -360,29 +360,37 @@ function getReviewsVideos() {
 function lazyLoad() {
     let lazyImages = [].slice.call(document.querySelectorAll(".lazy-load"));
 
+    lazyImages.forEach(function (lazyImage) {
+        if (!lazyImage.src) {
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.add("loaded");
+            lazyImage.removeAttribute("data-src");
+        }
+    });
 
-    if ("IntersectionObserver" in window) {
-        let lazyImageObserver = new IntersectionObserver(function (
-            entries,
-            observer
-        ) {
-            entries.forEach(function (entry) {
-                if (entry.isIntersecting) {
-                    let lazyImage = entry.target;
-                    if (!lazyImage.src) {
-                        lazyImage.src = lazyImage.dataset.src;
-                        lazyImage.classList.add("loaded");
-                        lazyImage.removeAttribute("data-src");
-                        lazyImageObserver.unobserve(lazyImage);
-                    }
-                }
-            });
-        });
 
-        lazyImages.forEach(function (lazyImage) {
-            lazyImageObserver.observe(lazyImage);
-        });
-    }
+    // if ("IntersectionObserver" in window) {
+    //     let lazyImageObserver = new IntersectionObserver(function (
+    //         entries,
+    //         observer
+    //     ) {
+    //         entries.forEach(function (entry) {
+    //             if (entry.isIntersecting) {
+    //                 let lazyImage = entry.target;
+    //                 if (!lazyImage.src) {
+    //                     lazyImage.src = lazyImage.dataset.src;
+    //                     lazyImage.classList.add("loaded");
+    //                     lazyImage.removeAttribute("data-src");
+    //                     lazyImageObserver.unobserve(lazyImage);
+    //                 }
+    //             }
+    //         });
+    //     });
+    //
+    //     lazyImages.forEach(function (lazyImage) {
+    //         lazyImageObserver.observe(lazyImage);
+    //     });
+    // }
 }
 
 function initSlickSliders() {
