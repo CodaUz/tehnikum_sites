4a8d81ba7551843e5fd530ab4fb193de829d0201
Sergiy Nimerovsky <sergii.nimerovsky@gmail.com>
2022-04-08 20:40:16 +0300

    build: change course

--- src/services/main_script.js
+++ src/services/main_script.js
@@ -614,6 +614,50 @@ function initSliders() {
         })
 }
 
+async function initCoursesInForm() {
+    const COURSES_ID = [[14, 'graphicDesignDate'], [10, 'smmForBeginnersDate'], [9, 'smmDate']]
+    const COURSES_THREADS_ID = [[25, 'digitalDate']]
+
+    for (let course_id of COURSES_ID) {
+        let res = await axios.get('https://api.tehnikum.uz/course.php', {
+            params: {
+                action: 'get',
+                token: '123',
+                id: course_id[0]
+            }
+        })
+        res = res['data']['row']
+        const first_date =  moment(`${res['date']}`, 'YYYY-MM-DD')
+        const first_date_format = first_date.locale("ru").format('D MMMM')
+        $(`.${course_id[1]}`).text(first_date_format)
+    }
+
+    for (let course_id of COURSES_THREADS_ID) {
+        let date = ''
+
+        let res = await fetch('https://tg-api.tehnikum.school/tehnikum_students/api/get_course_date_start', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                course_id: course_id[0]
+            })
+        });
+        res = await res.json();
+        res = res['data']
+
+        if (res['date_start']) {
+            date = res['date_start']
+        }
+
+        const first_date =  moment(`${date}`, 'YYYY-MM-DD')
+        const first_date_format = first_date.locale("ru").format('D MMMM')
+        $(`.${course_id[1]}`).text(first_date_format)
+    }
+}
+
 function init() {
     initSliders()
     setCountDown()
@@ -630,6 +674,7 @@ function init() {
     getMaxWidth()
     lazyLoad()
     listenPhoneInputs()
+    initCoursesInForm()
 
     document.querySelector(".loader").classList.add("active");
     setTimeout(() => {
